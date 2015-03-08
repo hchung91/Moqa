a60f4b05967a93a0d801136ac531b0e1c2f36794
kevingin9 <kevingin9@gmail.com>
2015-03-07 21:57:14 -0700

    reverted back app.js and built project

--- moqa/app.js
+++ moqa/app.js
@@ -1,38 +1,12 @@
 var express = require('express');
-var http = require('http');
 var path = require('path');
-var favicon = require('static-favicon');
+var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var mongo = require('mongodb');
-var mongoose = require('mongoose');
-var db = mongoose.connection;
-
-db.on('error',console.error);
-db.once('open',function(){
-	var commentSchema = new mongoose.Schema({
-		_id: mongoose.Schema.Types.ObjectId,
-		discussion_id: mongoose.Schema.Types.ObjectId,
-		parent_id: mongoose.Schema.Types.ObjectId,
-		slug: String,
-		full_slug: String,
-		timestamp: { type: Date, default: Date.now },
-		text: String,
-		author: mongoose.Schema.Types.ObjectId
-	})
-	
-	var comment = mongoose.model('Comment',commentSchema);
-	
-	
-});
-
-
-
-mongoose.connect('mongodb://localhost/test');
 
 var routes = require('./routes/index');
-var users = require('./routes/user');
+var users = require('./routes/users');
 
 var app = express();
 
@@ -40,53 +14,47 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(favicon());
+// uncomment after placing your favicon in /public
+//app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(app.router);
 
+app.use('/', routes);
+app.use('/users', users);
 
-
-app.get('/', routes.index);
-app.get('/users', users.list);
-app.get('/helloworld', routes.helloworld);
-
-
-/// catch 404 and forwarding to error handler
+// catch 404 and forward to error handler
 app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
-/// error handlers
+// error handlers
 
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
     });
+  });
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
 });
 
-
-app.get('/helloworld', function (req, res) {
-  res.send('Hello World!');
-})
 
 module.exports = app;
