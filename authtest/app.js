aed861974a13a1d369656f3fa7d331d62d2c2c7f
hchung91 <h.chung91@gmail.com>
2015-03-09 06:44:52 -0700

    authorization
    added authorization/authentication in authtest
    can make comments by user
    still trying to figure out how to find associated comments and display

---
+++ authtest/app.js
@@ -0,0 +1,162 @@
+var bodyParser = require('body-parser');
+var express = require('express');
+var bcrypt = require('bcryptjs')
+//var csrf = require('csurf');
+var mongoose = require('mongoose');
+var sessions = require('client-sessions');
+
+var Schema = mongoose.Schema;
+var ObjectId = Schema.ObjectId;
+
+//connect to mongo
+var User = mongoose.model('moqaUser', new Schema({
+	id: ObjectId,
+	moqaName: String,
+	firstName: String,
+	lastName: String,
+	email: {type: String, unique: true},
+	password: String,
+}));
+
+var Comment = mongoose.model('moqaComment', new Schema({
+	id: ObjectId,
+	moqaName: String,
+	commentTimeStamp: Date,
+	commentContent: String,
+}));
+
+var app = express();
+app.set('view engine', 'jade');
+app.locals.pretty = true;
+
+mongoose.connect('mongodb://localhost/mongoMoqaDB');
+
+
+// MIDDLEWARE
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(sessions({
+	cookieName: 'session',
+	secret:'jaslsad9fa87dfj234asdfasd9f8ah98ysdf',
+	duration: 30*60*1000,
+	activeDuration: 5*60*1000,
+	httpOnly: true, //don't let browser js ever access cookies
+	secure: true, // only use cookies over https
+	ephemeral: true, //delete this cookie when the broswer is closed
+	// also look at passportjs and drywall
+}));
+
+//app.use(express.csrf());
+
+app.use(function(req, res, next){
+	if (req.session && req.session.user){
+		User.findOne({email: req.session.user.email}, function(err, user){
+			if(user){
+				req.user = user;
+				delete req.user.password;
+				req.session.user = req.user;
+				res.locals.user = req.user;
+			}
+			next();
+		});
+	}
+	else{
+		next();
+	}
+});
+
+function requireLogin(req, res, next){
+	if(!req.user){
+		res.redirect('/login');
+	}
+	else{
+		next();
+	}
+};
+
+
+
+// VIEWS
+app.get('/', function(req,res){
+	//res.render('index.jade', {csrfToken: req.csrfToken()});
+	res.render('index.jade');
+});
+
+app.get('/register', function(req,res){
+	res.render('register.jade');
+})
+
+app.post('/login', function(req,res){
+	User.findOne({email:req.body.email}, function(err, user){
+		if (!user){
+			res.render('login.jade', {error: 'Invalid email or password!'})
+		}
+		else{
+			if (bcrypt.compareSync(req.body.password, user.password)){
+				req.session.user = user;
+				res.redirect('/dashboard')
+			}
+			else{
+				res.render('login.jade', {error: 'Invalid email or password!'})
+			}
+		}
+	});
+});
+
+app.post('/register', function(req,res){
+	var hash = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10));
+
+	var user = new User({
+		moqaName: req.body.moqaName,
+		firstName: req.body.firstName,
+		lastName: req.body.lastName,
+		email: req.body.email,
+		password: hash,
+	});
+	user.save(function(err){
+		if (err){
+			var err = 'Something bad happened! Try again!';
+			if (err.code === 11000){
+				error = 'That email is already taken.'
+			}
+			res.render('register.jade', {error: error});
+		}
+		else{
+			res.redirect('/dashboard');
+		}
+	})
+});
+
+app.get('/login', function(req,res){
+	//res.render('login.jade', {csrfToken: req.csrfToken()});
+	res.render('login.jade');
+})
+
+app.get('/dashboard', requireLogin, function(req,res){
+	var temp = Comment.findOne({moqaName: req.session.user.moqaName});
+	res.locals.recentComment = temp.commentContent;
+	res.render('dashboard.jade');
+})
+
+app.post('/dashboard', function(req,res){
+	var comment = new Comment({
+		moqaName: req.session.user.moqaName,
+		commentTimeStamp: Date.now(),
+		commentContent: req.body.commentContent,
+	})
+	comment.save(function(err){
+		if(err){
+			var err = 'Something bad happened! Try again!';
+		}
+		else{
+			res.redirect('/dashboard');
+		}
+	})
+})
+
+app.get('/logout', function(req,res){
+	req.session.reset();
+	res.render('index.jade');
+})
+
+app.listen(3000);
