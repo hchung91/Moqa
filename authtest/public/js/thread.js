919527dc6d27571f2777bc3a8723a9fe3bbab2a0
hchung91 <h.chung91@gmail.com>
2015-03-15 03:28:27 -0700

    style updates
    minor style changes

--- authtest/public/js/thread.js
+++ authtest/public/js/thread.js
@@ -119,21 +119,15 @@ function findParents(comments, callback){
 function timePassed(date){
 	var seconds = Math.floor(((new Date().getTime()/1000) - new Date(date).getTime()/1000)),
 	interval = Math.floor(seconds / 31536000);
-
 	if (interval > 1) return "   " +interval + " years ago";
-
 	interval = Math.floor(seconds / 2592000);
 	if (interval > 1) return "   " +interval + " minutes ago";
-
 	interval = Math.floor(seconds / 86400);
 	if (interval >= 1) return "   " +interval + " days ago";
-
 	interval = Math.floor(seconds / 3600);
 	if (interval >= 1) return "   " +interval + " hours ago";
-
 	interval = Math.floor(seconds / 60);
 	if (interval > 1) return "   " +interval + " minutes ago";
-
 	return Math.floor(seconds) + " seconds ago";
 }
 
